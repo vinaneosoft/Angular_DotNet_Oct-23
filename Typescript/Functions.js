4680d4afee63a5ff254202374a7bd4bd8abd04fa
kokaneva <kokane.vina@gmail.com>
2023-10-17 15:12:34 +0530

    filter

--- Typescript/Functions.js
+++ Typescript/Functions.js
@@ -21,3 +21,18 @@ var add3 = function (num1, num2) { return console.log(num1 + num2); };
 add3(45, 67);
 var cubeFun = function (num) { return num * num * num; }; // automatically returned
 console.log(cubeFun(3));
+// Consider following array
+var salaryarray = [56000, 98000, 67000, 45000, 56000, 34000, 67000, 89000];
+// store salaries greater than 60000 in new array and display new array
+// implement using ur own logic 
+// output [98000,67000,67000,89000]
+var filteredArray = [];
+for (var _i = 0, salaryarray_1 = salaryarray; _i < salaryarray_1.length; _i++) {
+    var salary = salaryarray_1[_i];
+    if (salary > 60000)
+        filteredArray.push(salary);
+}
+console.log(filteredArray);
+// filter method
+var array2 = salaryarray.filter(function (salary) { return salary > 60000; });
+console.log(array2);
--- Typescript/Functions.ts
+++ Typescript/Functions.ts
@@ -31,3 +31,23 @@ add3(45,67);
 const cubeFun=num=>num*num*num;  // automatically returned
 
 console.log(cubeFun(3));
+
+
+
+
+// Consider following array
+const salaryarray=[56000,98000,67000,45000,56000,34000,67000,89000];
+// store salaries greater than 60000 in new array and display new array
+// implement using ur own logic 
+
+// output [98000,67000,67000,89000]
+let filteredArray:number[]=[];
+for(let salary of salaryarray){
+    if(salary>60000)
+        filteredArray.push(salary)
+}
+console.log(filteredArray);
+
+// filter method
+const array2=salaryarray.filter(salary=>salary>60000)
+console.log(array2);
