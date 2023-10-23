9aa0c2478753b5600abe0094af5b61a30ab94c07
kokanevina <kokane.vina@gmail.com>
2023-10-23 17:37:11 +0530

    readonly

--- Typescript/3_Inheritance/MultipleDemo.ts
+++ Typescript/3_Inheritance/MultipleDemo.ts
@@ -14,3 +14,5 @@ class Child extends Father,Mother{
        return  this.dream(); // inheritance ambiguity
     }
 }
+
+
---
+++ Typescript/3_Inheritance/readonlydemo.ts
@@ -0,0 +1,26 @@
+const city="mumbai";
+
+
+class Employee{
+    readonly employeeId:number; // instance var. constants
+    employeeName:string;
+    employeeSalary:number;
+    static readonly companyName="Neosoft"; // single shared constants
+    constructor(employeeId=0,employeeName="AAA",employeeSalary=0){
+        this.employeeId=employeeId;
+        this.employeeName=employeeName;
+        this.employeeSalary=employeeSalary;
+    }
+
+    getMonthlySalary():number{   // bydefault public
+        return this.employeeSalary;
+    }
+}
+//Employee.companyName="Neosoft Pvt Ltd"; //Cannot assign to 'companyName' because it is a read-only property.
+
+let emp1=new Employee(1,"Vina",78000);
+let emp2=new Employee(2,"Amol",88000);
+let emp3=new Employee(3,"Sarika",78000);
+let emp4=new Employee(4,"Kumar",90000);
+
+//emp1.employeeId=8; //Cannot assign to 'employeeId' because it is a read-only property.
