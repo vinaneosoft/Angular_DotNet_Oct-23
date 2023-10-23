45d718054a86cfae6a7439f22cee8c99f7a19755
kokanevina <kokane.vina@gmail.com>
2023-10-23 11:53:21 +0530

    shortcut

---
+++ Typescript/2_ClassesandObjects/CostructorDemo2.js
@@ -0,0 +1,52 @@
+var BankAccount = /** @class */ (function () {
+    // more than one constructor not allowed in typescipt class
+    function BankAccount(accountNumber, customerId, accountType, accountBalance) {
+        if (accountNumber === void 0) { accountNumber = 0; }
+        if (customerId === void 0) { customerId = 0; }
+        if (accountType === void 0) { accountType = "Current"; }
+        if (accountBalance === void 0) { accountBalance = 0; }
+        this.accountNumber = accountNumber;
+        this.customerId = customerId;
+        this.accountType = accountType;
+        this.accountBalance = accountBalance;
+    }
+    Object.defineProperty(BankAccount.prototype, "accNum", {
+        get: function () {
+            return this.accountNumber;
+        },
+        set: function (accountNumber) {
+            this.accountNumber = accountNumber;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(BankAccount.prototype, "custId", {
+        get: function () {
+            return this.customerId;
+        },
+        set: function (customerId) {
+            this.customerId = customerId;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    //instance method, camel case
+    BankAccount.prototype.deposit = function (depAmount) {
+        this.accountBalance = this.accountBalance + depAmount;
+        return this.accountBalance;
+    };
+    // implement withdraw method
+    BankAccount.prototype.getDetails = function () {
+        // return "Account Number:"+this.accountNumber+" Customer Id:"+this.customerId+" Account Type:"+this.accountType+" balance:"+this.accountBalance
+        return "Account Number : ".concat(this.accountNumber, "\n                Customer Id: ").concat(this.customerId, "\n                type: ").concat(this.accountType, "\n                balance: ").concat(this.accountBalance);
+    };
+    return BankAccount;
+}());
+// new keyword : new object created
+var account1; // custom types
+account1 = new BankAccount(7878787878, 111111, "Savings", 10000); // p constructor called   //1.
+console.log(account1);
+var account2 = new BankAccount(8898787878, 121212, "Savings", 15000);
+console.log(account2);
+var account3 = new BankAccount();
+console.log(account3);
---
+++ Typescript/2_ClassesandObjects/CostructorDemo2.ts
@@ -0,0 +1,41 @@
+
+class BankAccount{
+    // shortcut to declare and initialize instance variables in one place
+    constructor(private accountNumber=0,private customerId=0,public accountType="Current",public accountBalance=0){
+    }
+    get accNum(){
+        return this.accountNumber;
+    }
+    set accNum(accountNumber){
+        this.accountNumber=accountNumber;
+    }
+    get custId(){
+        return this.customerId;
+    }
+    set custId(customerId){
+        this.customerId=customerId;
+    }
+    //instance method, camel case
+    deposit(depAmount:number):number
+    {
+        this.accountBalance=this.accountBalance+depAmount;
+        return this.accountBalance;
+    }
+    // implement withdraw method
+    getDetails():string{
+       // return "Account Number:"+this.accountNumber+" Customer Id:"+this.customerId+" Account Type:"+this.accountType+" balance:"+this.accountBalance
+        return `Account Number : ${this.accountNumber}
+                Customer Id: ${this.customerId}
+                type: ${this.accountType}
+                balance: ${this.accountBalance}`
+    }
+}
+// new keyword : new object created
+
+let account1:BankAccount // custom types
+account1=new BankAccount(7878787878,111111,"Savings",10000);  // p constructor called   //1.
+console.log(account1);
+let account2=new BankAccount(8898787878,121212,"Savings",15000);
+console.log(account2);
+let account3=new BankAccount();
+console.log(account3);
