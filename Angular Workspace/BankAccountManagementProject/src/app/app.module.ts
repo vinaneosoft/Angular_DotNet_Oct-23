ceaba8939d42415afff6b8ddac90824654ed4ee5
kokanevina <kokane.vina@gmail.com>
2023-10-30 17:48:58 +0530

    commit

--- Angular Workspace/BankAccountManagementProject/src/app/app.module.ts
+++ Angular Workspace/BankAccountManagementProject/src/app/app.module.ts
@@ -4,9 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
 @NgModule({
   declarations: [
-    AppComponent // other declarations of components goes here
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent // other declarations of components goes here
   ],
   imports: [
     BrowserModule,
--- Angular Workspace/employeemanagementproject/src/app/app.component.ts
+++ Angular Workspace/employeemanagementproject/src/app/app.component.ts
@@ -3,13 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css', './test.css']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  bankName="AXIS BANK";
-  branch:string;
-
-  constructor(){
-    this.branch="RABALE";
-  }
+  title = 'employeemanagementproject';
 }
