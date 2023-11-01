6dd1a605f01efbff7fb0d9c017c3b4d462e1fc54
kokanevina <kokane.vina@gmail.com>
2023-11-01 13:02:02 +0530

    strings pipes, number pipes

--- Angular Workspace/BankAccountManagementProject/src/app/classes/bank-account.ts
+++ Angular Workspace/BankAccountManagementProject/src/app/classes/bank-account.ts
@@ -1,5 +1,5 @@
 export class BankAccount {
-    constructor(private accountNumber=0,private customerId=0,private accountBalance=0,private accountType="savings")
+    constructor(private accountNumber=0,private customerId=0,private customerName="AAA",private accountBalance=0,private accountType="savings")
     {}
     get accNum(){
         return this.accountNumber;
@@ -13,6 +13,9 @@ export class BankAccount {
     get accType(){
         return this.accountType;
     }
+    get custName(){
+        return this.customerName;
+    }
     set accNum(accountNumber:number){
         this.accountNumber=accountNumber
     }
@@ -24,6 +27,9 @@ export class BankAccount {
     }
     set accType(accountType:string){
         this.accountType=accountType
+    }
+    set custName(customerName:string){
+        this.customerName=customerName;
     }
     deposit(deptAmount:number){
 
