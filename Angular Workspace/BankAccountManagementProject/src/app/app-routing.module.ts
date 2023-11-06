582aa768c0c3a0d75be92dba6583243b15a17b0e
kokanevina <kokane.vina@gmail.com>
2023-11-06 12:42:23 +0530

    routing

--- Angular Workspace/BankAccountManagementProject/src/app/app-routing.module.ts
+++ Angular Workspace/BankAccountManagementProject/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { BankAccountComponent } from './bankaccount/bankaccount.component';
+import { BranchDetailsComponent } from './branch-details/branch-details.component';
 
 const routes: Routes = [
   { component:HomeComponent, path:'home'},
   { component:LoginComponent, path:'adminlogin'},
-  { component:BankAccountComponent, path:'bankaccounts'}
+  { component:BankAccountComponent, path:'bankaccounts'},
+  { component:BranchDetailsComponent, path:'branchdetails/:accno'}
 
 ];
 
