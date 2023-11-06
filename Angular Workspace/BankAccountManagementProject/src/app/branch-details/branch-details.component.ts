3463f1a483be7a15d49dbc10868c696d0ad9d578
kokanevina <kokane.vina@gmail.com>
2023-11-06 13:15:25 +0530

    commit

--- Angular Workspace/BankAccountManagementProject/src/app/branch-details/branch-details.component.ts
+++ Angular Workspace/BankAccountManagementProject/src/app/branch-details/branch-details.component.ts
@@ -8,11 +8,10 @@ import { ActivatedRoute } from '@angular/router'; // built in service, API to se
   styleUrls: ['./branch-details.component.css']
 })
 export class BranchDetailsComponent {
-  accountNumber=0;
-
-    rabaleBranch=new Branch('AR2344', 'Axis Bank, Rabale', 'Plot no.3, MG Road, Rabale', '9898989898')
-    belapurBranch=new Branch('AB2344', 'Axis Bank, Belapur', 'Plot no.1, Prabhat Road, Belapur', '909090909')
-    vashiBranch=new Branch('AV2344', 'Axis Bank, Vashi', 'Plot no.2, Jahangir Road, Vashi', '8080808080')
+  accountNumber=0; 
+  rabaleBranch=new Branch('AR2344', 'Axis Bank, Rabale', 'Plot no.3, MG Road, Rabale', '9898989898')
+  belapurBranch=new Branch('AB2344', 'Axis Bank, Belapur', 'Plot no.1, Prabhat Road, Belapur', '909090909')
+  vashiBranch=new Branch('AV2344', 'Axis Bank, Vashi', 'Plot no.2, Jahangir Road, Vashi', '8080808080')
   accountBranchDetails:AccountBranch[]=[
     new AccountBranch(22222222,this.rabaleBranch),
     new AccountBranch(90909090,this.vashiBranch),
@@ -21,12 +20,13 @@ export class BranchDetailsComponent {
     new AccountBranch(56565656,this.rabaleBranch),
     new AccountBranch(23232323,this.rabaleBranch)
   ]
-
-
   constructor(private currentRoute:ActivatedRoute){ // when contructor gets called, service object gets injected automatically
      let routeParam=currentRoute.snapshot.params['accno'];  // route params always of type string 
      this.accountNumber=parseInt(routeParam);
      console.log(this.accountNumber);
-     
+  }
+  findBranch(){
+    // find record in accountBranchDetails matching with accountNumber passed from Route
+    // display object details on html page
   }
 }
