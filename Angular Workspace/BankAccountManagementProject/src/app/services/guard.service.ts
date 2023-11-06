c22ce75a3f7b46029b31031b1a48d50527d9390b
kokanevina <kokane.vina@gmail.com>
2023-11-06 17:42:15 +0530

    Routeguard

--- Angular Workspace/BankAccountManagementProject/src/app/services/guard.service.ts
+++ Angular Workspace/BankAccountManagementProject/src/app/services/guard.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { LoginService } from './login.service';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class GuardService implements CanActivate{
-  constructor() { }
+  constructor(private loginService:LoginService) { }
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-    return false;
+    if (this.loginService.loginFlag) 
+      return true;  // YES routes get activated : BankAccountComponent 
+    else{
+      window.alert("Please login to proceed.......");
+      return false; // NO route will not be activated
+    }
   }
  
 }
